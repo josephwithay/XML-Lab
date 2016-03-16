ac4c0505ad0c15d1629a3fcdc30af6ab438a64d8
josephwithay <yoseph_jo@hotmail.com>
2016-03-15 23:03:58 -0700

    Merge teamrepo/develop into BookingClass
    Conflicts:
    	changelog.txt
    Signed-off-by:josephwithay <yoseph_jo@hotmail.com>

--- application/models/Timetable.php
+++ application/models/Timetable.php
@@ -1,32 +1,64 @@
 <?php
+
 /**
  *  This is a model for the control data in our timetable slots
  */
-class Timetable extends CI_Model{
-    
-    protected $xml = null;
-    protected $days = array();
-    protected $timeslots = array();
-    protected $courses = array();
-    
-    
-    public function __construct() {
-        parent::__construct();
-        $this->xml = simplexml_load_file(DATAPATH . 'timetable.xml');
-        
-        //build a full list of days
-        foreach($this->xml->days->day as $day){
-            $this->days[] = new Booking($day);
-        }
-        
-        //build a full list of timeslots
-        foreach($this->xml->timeslots->timeslot as $time){
-            $this->timeslots[] = new Booking($time);
-        }
-        
-        //build a full list of courses
-        foreach($this->xml->courses->course as $course){
-            $this->courses[] = new Booking($course);
-        }
-    }
+class Timetable extends CI_Model
+{
+
+	protected $xml = null;
+	protected $days = array();
+	protected $timeslots = array();
+	protected $courses = array();
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->xml = simplexml_load_file(DATAPATH . 'timetable.xml');
+
+		//build a full list of days
+		foreach ($this->xml->days->day as $day)
+		{
+			$this->days[] = new Booking($day);
+		}
+
+		//build a full list of timeslots
+		foreach ($this->xml->timeslots->timeslot as $time)
+		{
+			$this->timeslots[] = new Booking($time);
+		}
+
+		//build a full list of courses
+		foreach ($this->xml->courses->course as $course)
+		{
+			$this->courses[] = new Booking($course);
+		}
+	}
+
+}
+
+Class Booking extends CI_Model
+{
+
+	public $weekday = "";
+	public $timeslot = "";
+	public $courseCode = "";
+	public $periodStart = "";
+	public $periodEnd = "";
+	public $room = "";
+	public $instructor = "";
+
+	// Constructor
+	public function __construct($detail)
+	{
+		parent::__construct();
+		$this->weekday = (string) $detail['weekday'];
+		$this->timeslot = (string) $detail['timeslot'];
+		$this->courseCode = (string) $detail['courseCode'];
+		$this->periodStart = (string) $detail['periodStart'];
+		$this->periodEnd = (string) $detail['periodEnd'];
+		$this->room = (string) $detail['room'];
+		$this->instructor = (string) $detail['instructor'];
+	}
+
 }
